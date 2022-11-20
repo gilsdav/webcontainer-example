b15cdf80e73f32f9fe7b17c2537bcf1a847e0f41
nontangent <irregular0number@gmail.com>
2022-11-20 19:25:29 +0900

    refactor: seperate loggger logic

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,92 @@
-import { load, FileSystemTree, WebContainer } from '@webcontainer/api';
+import { load, FileSystemTree, WebContainer } from '@webContainer/api';
 import LogConverter from 'ansi-to-html';
 
-const logConverter = new LogConverter();
+export class Logger {
+    private el = document.querySelector(this.selector);
+    private converter = new LogConverter();
+    private replace = false;
+
+    constructor(private selector: string) { }
+
+    log(data: string, namespace: string) {        
+        if (data.trim() === '\u001b[?25l') {
+            this.replace = true;
+            this.nextParagraph();
+            this.write('');
+        } else if (data === '\u001b[?25h') {
+            this.replace = false;
+        } else if (data !== '\u001b[1G') {
+            if(!this.replace) this.nextParagraph();
+            this.write(`[${namespace}] ${data}`);
+        } 
+    }
+
+    private nextParagraph() {
+        this.el.appendChild(document.createElement('p'));
+    }
+
+    private write(text: string) {
+        console.log(text);
+        this.el.lastElementChild.innerHTML = this.converter.toHtml(text);
+        this.el.scrollTop = this.el.scrollHeight;
+    }
+}
 
 export class Backend {
-
-    private webcontainer: WebContainer;
+    private webContainer: WebContainer;
 
     public initialized: Promise<WebContainer>;
 
-    private resolver: (value: unknown) => void;
-    public urlEmiter = new Promise((resolver) => {
-        this.resolver = resolver;
-    });
+    private emitUrl: (url: string) => void;
+    public urlEmitter = new Promise((emitUrl) => this.emitUrl = emitUrl);
 
-    constructor(private loggerElementSelector: string) {
+    constructor(private logger: Logger) {
         this.initialized = this.init();
     }
 
     async init() {
-        this.webcontainer = await this.bootWhenReady();
-        return this.webcontainer;
+        this.webContainer = await this.bootWhenReady();
+        return this.webContainer;
     }
 
     // call this only once
     async bootWhenReady() {
         // `load` should only be called once
-        const WebContainer = await load();
-    
+        const webContainer = await load();
+
         // only a single instance of WebContainer can be created
-        const webcontainer = await WebContainer.boot();
-    
-        return webcontainer;
+        return webContainer.boot();
     }
     
     // you'll need to "copy" your project files into the file system
     async initProject(projectFiles: FileSystemTree) {
-        return await this.webcontainer.loadFiles(projectFiles);
+        return this.webContainer.loadFiles(projectFiles);
+    }
+
+    private async runCommand({command, args}: {command: string, args: string[]}, namespace: string): Promise<string[]> {
+        return new Promise(async (resolve) => {
+            const outputs: string[] = [];
+            const process = await this.webContainer.run({command, args}, {
+                output: (data: string) => {
+                    outputs.push(data);
+                    this.logger.log(data, namespace);
+                },
+            });
+            const exitCode = await process.onExit;    
+            if (exitCode !== 0) throw new Error(`Unable to run ${command}`);
+            resolve(outputs);
+        });
     }
     
     async startDevServer() {
-        let  installReplace = false;
-        const install = await this.webcontainer.run(
-        {
-            command: 'npm',
-            args: ['i'],
-        },
-        {
-            output: (data: string) => {
-                console.log(`[npm install] ${data}`);
-                if (data.trim() === '\u001b[?25l') {
-                    installReplace = true;
-                    this.writeLog('', false);
-                } else if (data === '\u001b[?25h') {
-                    installReplace = false;
-                }
-                if (data !== '\u001b[?25h' && data !== '\u001b[?25l' && data !== '\u001b[1G') {
-                    this.writeLog(logConverter.toHtml(`[npm install] ${data}`), installReplace);
-                }
-                
-            },
-        }
-        );
-    
-        const installExitCode = await install.onExit;
-    
-        if (installExitCode !== 0) {
-            throw new Error('Unable to run npm install');
-        }
-
-        const urlResolver = await this.webcontainer.run({
-            command: 'node',
-            args: ['hostRelover.mjs']
-        },
-        {
-            output: (data: string) => {
-                console.log(`[Host resolver] ${data}`);
-                this.resolver(data);
-            },
-        }
-        );
-        
-        await urlResolver.onExit;
-    
-        return await this.webcontainer.run(
-        {
-            command: 'npm',
-            args: ['run', 'dev'],
-        },
-        {
-            output: (data: string) => {
-                console.log(`[dev server] ${data}`);
-                this.writeLog(logConverter.toHtml(`[dev server] ${data}`));
-            },
-        }
-        );
+        await this.runCommand({command: 'npm', args: ['i']}, 'npm install');
+        await this.runCommand({command: 'node', args: ['hostRelover.mjs']}, 'Host resolver')
+            .then(([url]) => this.emitUrl(url));
+        await this.runCommand({command: 'npm', args: ['run', 'dev']}, 'npm runDev');
     }
 
     public stop() {
-        this.webcontainer.teardown();
+        this.webContainer.teardown();
     }
-
-    private writeLog(log: string, replaceLatest = false) {
-        const logsContainer = document.querySelector(this.loggerElementSelector);
-        let p;
-        if (replaceLatest) {
-            p = logsContainer.lastElementChild;
-        } else {
-            p = document.createElement('p');
-            logsContainer.appendChild(p);
-        }
-        p.innerHTML = log;
-        logsContainer.scrollTop = logsContainer.scrollHeight;
-    }
-
 
 }
